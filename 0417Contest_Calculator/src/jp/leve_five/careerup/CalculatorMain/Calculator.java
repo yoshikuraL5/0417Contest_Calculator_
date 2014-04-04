06743c17f4a5ae11d0cf2445bb668c7ee7e2ff0e
yoshikura <yoshikura@192.168.116.6>
2014-04-04 16:26:55 +0900

    足し算、引き算実装済。 掛け算実装中

---
+++ 0417Contest_Calculator/src/jp/leve_five/careerup/CalculatorMain/Calculator.java
@@ -0,0 +1,17 @@
+package jp.leve_five.careerup.CalculatorMain;
+
+public class Calculator {
+	public int plus(int a,int b){
+		return a + b;
+	}
+
+	public int minus(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		return 1;
+	}
+
+
+}
---
+++ 0417Contest_Calculator/src/jp/leve_five/careerup/CalculatorTest/CalculatorTest.java
@@ -0,0 +1,67 @@
+package jp.leve_five.careerup.CalculatorTest;
+
+import static org.junit.Assert.*;
+import jp.leve_five.careerup.CalculatorMain.Calculator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+	Calculator calculator;
+
+	@Before
+	public void before() {
+		calculator = new Calculator();
+	}
+
+	@Test
+	public void test数字の１と１をplusメソッドに渡すと２が返る() {
+		assertEquals(2, calculator.plus(1, 1));
+	}
+	@Test
+	public void test数字の１と２をplusメソッドに渡すと３が返る() {
+		assertEquals(3, calculator.plus(1, 2));
+	}	
+	@Test
+	public void test数字の３と７をplusメソッドに渡すと１０が返る(){
+		assertEquals(10, calculator.plus(3, 7));
+	}
+	
+	@Test
+	public void test数字の６０と９４０をplusメソッドに渡すと１０００が返る(){
+		assertEquals(1000, calculator.plus(60, 940));
+	}
+	@Test
+	public void test数字のマイナス１と５００をplusメソッドに渡すと499が返る(){
+		assertEquals(499, calculator.plus(-1, 500));
+	}
+	@Test
+	public void test数字の２と１をminusメソッドに渡すと１が返る(){
+		assertEquals(1, calculator.minus(2, 1));
+	}
+	@Test
+	public void test数字の３と１をminusメソッドに渡すと２が返る(){
+		assertEquals(2, calculator.minus(3, 1));
+	}
+	@Test
+	public void test数字の２２と８をminusメソッドに渡すと１４が返る(){
+		assertEquals(14, calculator.minus(22, 8));
+	}
+	@Test
+	public void test数字の150と44をminusメソッドに渡すと106が返る(){
+		assertEquals(106, calculator.minus(150, 44));
+	}
+	@Test
+	public void test数字の1304と1111をminusメソッドに渡すと193が返る(){
+		assertEquals(193, calculator.minus(1304, 1111));
+	}
+	@Test
+	public void test数字の２とマイナス１００をminusメソッドに渡すとが102返る(){
+		assertEquals(102, calculator.minus(2, -100));
+	}
+	@Test
+	public void test数字の１と１をmultiplyメソッドに渡すと１が返る(){
+		assertEquals(1, calculator.multiply(1, 1));
+	}
+	
+}
